3666840df0904e34052c094de7cab1a0ea5921b3
Ladislas <ladislas@leka.io>
2015-07-16 18:08:31 +0200

    make bootstrap script zsh compatible and improve messages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env zsh
+#
 
-cd "$(dirname "${BASH_SOURCE}")";
+SCRIPT_PATH=${0}
+cd "$(dirname "${SCRIPT_PATH}")";
 
 git pull origin master;
 
 function doIt() {
 
-	rsync --exclude-from='./.rsyncexclude' -avh --no-perms . ~;
-	source ~/.zshrc;
+	# rsync --exclude-from='./.rsyncexclude' -avh --no-perms . ~;
+	# source ~/.zshrc;
+	echo -n "\n$fg[cyan]Press any key to quit and restart open a new terminal window..."
+	read quit
+	exit;
 
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [[ $@ == "--force" || $@ == "-f" ]]; then
 	doIt;
 else
-	echo -n "$fg[red]This may overwrite existing files in your home directory. Are you sure? (y/n)$fg[green] "
+	echo -n "\n$fg[red]This may overwrite existing files in your home directory. Are you sure? (y/n)$fg[green] "
 	read REPLY
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		echo "Ok!";
 		doIt;
 	fi;
 fi;
