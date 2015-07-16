813d4aed70e2dae40a3d18754c7f6094450706a0
Ladislas <ladislas@leka.io>
2015-07-16 10:25:52 +0200

    add rsyncexclude pattern file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".gitmodules" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "brew.sh" --exclude ".zsh" --exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
+
+	rsync --exclude-from='./.rsyncexclude' -avh --no-perms . ~;
 	source ~/.zshrc;
+
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
+	echo -n "$fg[red]This may overwrite existing files in your home directory. Are you sure? (y/n)$fg[green] "
+	read REPLY
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "Ok!";
 		doIt;
 	fi;
 fi;
